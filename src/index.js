57e5723ccb481e0b9a48b841b5d12a3365088823
adem <ademlejmi23@gmail.com>
2023-12-28 10:45:58 +0100

    second commit

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import {BrowserRouter} from 'react-router-dom'
 import { Provider } from 'react-redux';
 import store from './redux/store';
 import axios from 'axios';
-axios.defaults.baseURL("https://server-6j7p.onrender.com/api")
+axios.defaults.baseURL="https://server-6j7p.onrender.com/api"
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
